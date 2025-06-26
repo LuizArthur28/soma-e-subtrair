6e00c46ed91980f89974ba45c6386ce07e996f37
LuizArthur28 <luiz.weiss@escola.pr.gov.br>
2025-06-26 08:44:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,12 +9,37 @@ function soma(){
 
 }
 
-function subtrai_numero(){
-   let numero_um = parseInt (document.getElementById("primeiro_numero"). value);
-   let numero_dois = parseInt (document. getElementById ("segundo_numero"). value);
+function subtrai(){
+   let numero_um = parseInt(document.getElementById("primeiro_numero"). value);
+   let numero_dois = parseInt(document. getElementById("segundo_numero"). value);
    let subtrai_numero = numero_um - numero_dois ;
   
-   document.getElementById("resultado"). textContent = "Resultado: " + subtraçao;
+   document.getElementById("resultado"). textContent = "Resultado: " + subtrai_numero;
 
 }
+function multiplicar(){
+   let numero_um = parseInt(document.getElementById("primeiro_numero"). value);
+   let numero_dois = parseInt(document. getElementById("segundo_numero"). value);
+   let multiplicar_numero = numero_um * numero_dois ;
+  
+   document.getElementById("resultado"). textContent = "Resultado: " + multiplicar_numero;
 
+}
+function dividir(){
+   let numero_um = parseInt(document.getElementById("primeiro_numero"). value);
+   let numero_dois = parseInt(document. getElementById("segundo_numero"). value);
+   
+   if(numero_dois == 0){
+   alert("Não pode dividir por zero, mané")
+  return;
+   }
+   let dividir_numero = numero_um / numero_dois ;
+  document.getElementById("resultado"). textContent = "Resultado: " + dividir_numero
+}
+function maior(){
+   let numero_um = parseInt(document. getElementById("primeiro_numero"). value);
+   let numero_dois = parseInt(document. getElementById("segundo_numero"). value);
+    if(numero_um>numero_dois){
+      document.getElementById("resultado").textcontent = 
+    }
+}
